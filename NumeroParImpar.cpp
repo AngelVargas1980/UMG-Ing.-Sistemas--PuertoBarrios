670e2aea0f50a7d703b91ab788fa3401a66b44de
AngelVargas1980 <49385201+AngelVargas1980@users.noreply.github.com>
2021-03-31 21:39:11 -0600

    subiendo archivo Numero impar

---
+++ NumeroParImpar.cpp
@@ -0,0 +1,19 @@
+//Esto refleja numero par  impar
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num;
+	cout<<"Introduzca un numero: "; cin>>num;
+	if ((num%2) == 0)
+		cout<< "Par" <<endl;
+		
+	else
+		cout<<"IMPAR"<<endl;
+		
+	system("pause");
+	
+	
+}
